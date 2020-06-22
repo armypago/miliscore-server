0880bba24342783f3b6ffc5674f934393bb04af9
Unknown <cupjoo@naver.com>
2020-06-22 18:27:22 +0900

    add EvaluationRepository test

--- src/main/java/com/armypago/miliscoreserver/evaluation/EvaluationRepository.java
+++ src/main/java/com/armypago/miliscoreserver/evaluation/EvaluationRepository.java
@@ -3,5 +3,9 @@ package com.armypago.miliscoreserver.evaluation;
 import com.armypago.miliscoreserver.domain.evaluation.Evaluation;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.util.Optional;
+
 public interface EvaluationRepository extends JpaRepository<Evaluation, Long> {
+
+    Optional<Evaluation> findByAuthorIdAndBranchId(Long authorId, Long branchId);
 }
--- src/test/java/com/armypago/miliscoreserver/evaluation/EvaluationRepositoryTest.java
+++ src/test/java/com/armypago/miliscoreserver/evaluation/EvaluationRepositoryTest.java
@@ -1,54 +1,76 @@
-package com.armypago.miliscoreserver.domain.evaluation;
+package com.armypago.miliscoreserver.evaluation;
 
-import com.armypago.miliscoreserver.branch.BranchRepository;
 import com.armypago.miliscoreserver.domain.branch.Branch;
+import com.armypago.miliscoreserver.domain.evaluation.Evaluation;
+import com.armypago.miliscoreserver.domain.evaluation.RadarChart;
 import com.armypago.miliscoreserver.domain.user.Education;
 import com.armypago.miliscoreserver.domain.user.MilitaryServiceStatus;
 import com.armypago.miliscoreserver.domain.user.User;
-import com.armypago.miliscoreserver.evaluation.EvaluationRepository;
 import com.armypago.miliscoreserver.user.EducationRepository;
-import com.armypago.miliscoreserver.user.UserRepository;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.transaction.annotation.Transactional;
 
+import javax.persistence.EntityManager;
+
 import static org.assertj.core.api.Assertions.assertThat;
 
 @Transactional
 @SpringBootTest
-class EvaluationTest {
+class EvaluationRepositoryTest {
 
-    @Autowired BranchRepository branchRepository;
-    @Autowired UserRepository userRepository;
     @Autowired EducationRepository educationRepository;
     @Autowired EvaluationRepository evaluationRepository;
-    
+    @Autowired EntityManager em;
+
     @Test
-    @DisplayName("한 줄 평가 생성")
+    @DisplayName("평가 생성/조회")
     void createAndUpdateEvaluation(){
         Branch branch = getBranch();
         User user = getUser(branch);
         String content = "개꿀입니다!";
+        RadarChart score = getScore(new double[]{1,1,1,1,1,60});
+        
         Evaluation evaluation = evaluationRepository.save(Evaluation.builder()
-                .author(user).branch(branch).content(content).build());
+                .score(score).author(user).branch(branch).content(content).build());
         evaluationRepository.findById(evaluation.getId()).ifPresent(e -> {
             assertThat(e.getAuthor().getName()).isEqualTo(user.getName());
             assertThat(e.getBranch().getName()).isEqualTo(branch.getName());
             assertThat(e.getContent()).isEqualTo(content);
         });
+        boolean byAuthorAndBranch = evaluationRepository
+                .findByAuthorIdAndBranchId(user.getId(), branch.getId())
+                .isPresent();
+        assertThat(byAuthorAndBranch).isTrue();
+    }
+
+    private RadarChart getScore(double[] score){
+        return RadarChart.builder()
+                .careerRelevance(score[0])
+                .workLifeBalance(score[1])
+                .unitVibe(score[2])
+                .trainingIntensity(score[3])
+                .officer(score[4])
+                .dayOfLeaves(score[5]).build();
     }
 
     private Branch getBranch() {
-        return branchRepository.save(Branch.builder().name("SW개발병").build());
+        Branch branch = Branch.builder().name("SW개발병").build();
+        em.persist(branch);
+        return branch;
     }
 
     private User getUser(Branch branch) {
-        Education education = educationRepository.save(Education.builder().priority(3).name("대졸").build());
-        return userRepository.save(User.builder()
+        Education education = educationRepository.findByPriority(3)
+                .orElseGet(()->educationRepository.save(Education.builder()
+                        .priority(3).name("대졸").build()));
+        User user = User.builder()
                 .name("junyoung").email("cupjoo@gmail.com")
                 .major("software").status(MilitaryServiceStatus.SERVING)
-                .branch(branch).education(education).build());
+                .branch(branch).education(education).build();
+        em.persist(user);
+        return user;
     }
 }
