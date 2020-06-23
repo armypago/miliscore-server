eec7d57182c17c7a0d53efbe3429a453233652d0
Unknown <cupjoo@naver.com>
2020-06-23 23:06:09 +0900

    update MilitaryServiceStatus type

--- src/main/java/com/armypago/miliscoreserver/domain/user/MilitaryServiceStatus.java
+++ src/main/java/com/armypago/miliscoreserver/domain/user/MilitaryServiceStatus.java
@@ -1,5 +1,16 @@
 package com.armypago.miliscoreserver.domain.user;
 
+import lombok.Getter;
+import lombok.RequiredArgsConstructor;
+
+@Getter
+@RequiredArgsConstructor
 public enum MilitaryServiceStatus {
-    DISCHARGED, SERVING, NOTJOINED
+
+    NOTJOINED("NOTJOINED", "미필"),
+    SERVING("SERVING", "복무중"),
+    DISCHARGED("DISCHARGED", "군필");
+    
+    private final String key;
+    private final String name;
 }
